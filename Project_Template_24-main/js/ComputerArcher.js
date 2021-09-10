88bd4360214dc407b7c8cd2bc6a17f4dc61948d3
shivanipt <87143211+shivanipt@users.noreply.github.com>
2021-09-10 15:17:15 -0500

    Add files via upload

---
+++ Project_Template_24-main/js/ComputerArcher.js
@@ -0,0 +1,39 @@
+class ComputerArcher {
+    constructor(x, y, width, height) {
+        var options = {
+            isStatic: true
+        };
+
+        this.body = Matter.Bodies.rectangle(x, y, width, height, options);
+        this.width = width;
+        this.height = height;
+        this.image = loadImage("./assets/computerArcher.png");
+
+        World.add(world, this.body);
+
+        Matter.Body.setAngle(this.body, -PI/2);
+    }
+    display() {
+        var position = this.body.position;
+        var angle = this.body.angle;
+
+        if(move === "UP" && ComputerArcher.body.angle < 1.77) {
+            angleValue = 0.1;
+        } else {
+            angleValue = -0.1;
+        }
+
+        if(move === "DOWN" && ComputerArcher.body.angle > 1.47) {
+            angle.value = -0.1;
+        } else {
+            angleValue = 0.1;
+        }
+
+        push();
+        translate(position.x, position.y);
+        rotate(angle);
+        imageMode(CENTER);
+        image(this.image, 0, 0, this.width, this.height);
+        pop();
+    }
+}
